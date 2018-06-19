96b2c42e2d5e5adf930b2e861aa1cd1f1fbe499e
Sudeep Jathar <sudeep.jathar@druva.com>
2018-06-19 10:48:04 +0530

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='Alchemy',
+    version='0.1.0',
+    author='Sudeep Jathar',
+    author_email='sudeep.jathar@gmail.com',
+    url='https://github.com/sudeep9/alchemy',
+    description='Alchemy',
+
+    packages=['alchemy'],
+    install_requires=['PyYaml'],
+    scripts=['alch']
+)
