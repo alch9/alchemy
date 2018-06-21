d3af17c6b3fb3e23079668cf4123a68fc3f1ea88
Sudeep Jathar <sudeep.jathar@druva.com>
2018-06-21 11:17:10 +0530

    Added readme, version info

---
+++ README.md
@@ -0,0 +1,5 @@
+# Alchemy - Automation framework
+
+# Installation
+
+ad
--- setup.py
+++ setup.py
@@ -1,15 +1,23 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name='Alchemy',
-    version='0.1.0',
+    version='0.2.0',
     author='Sudeep Jathar',
     author_email='sudeep.jathar@gmail.com',
     url='https://github.com/sudeep9/alchemy',
-    description='Alchemy',
+    description='Simple and general puprose automation framework',
+    license='MIT',
+    classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 2.7',
+          'Operating System :: POSIX :: Linux'
+    ],
 
     packages=['alchemy'],
-    install_requires=['PyYaml'],
+    install_requires=['PyYaml', 'docopt'],
     scripts=['alch'],
     package_data = {
         'alchemy': ["*.yml"]
