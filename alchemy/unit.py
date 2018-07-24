cc61301e2da7bf76857eb17463f1f92a7a1cac94
Sudeep Jathar <sudeep.jathar@gmail.com>
2018-07-24 23:30:09 +0530

    Started enforcing

--- alchemy/unit.py
+++ alchemy/unit.py
@@ -1,5 +1,5 @@
 
-import inspect
+import os, inspect
 import importlib
 
 UNIT_TYPE_SIMPLE = 1
@@ -16,6 +16,7 @@ class FunctionUnit(Unit):
         Unit.__init__(self)
         self.module = None
         self.func = None
+        self.pos_args = None
         self.args = None
         self.kargs = None
         self.input_desc = {}
@@ -134,6 +135,7 @@ def create_derived_unit(name, input, output, defaults, ui_list):
     u.output = output
     u.ui_list = ui_list
     u.defaults = defaults
+    u.unit_type = UNIT_TYPE_DERIVED
 
     return u
 
@@ -143,8 +145,10 @@ def create_unit_inst_from_dict(d):
     return UnitInstance(name, params)
 
 def create_derived_unit_from_dict(name, d):
-    unit_input = d.get('input', None)
-    unit_output = d.get('output', None)
+    check_derived_unit(name, d)
+
+    unit_input = d.get('input', {})
+    unit_output = d.get('output', {})
     unit_defaults = d.get('defaults', {})
 
     ui_list = []
@@ -175,11 +179,11 @@ def _get_func_args(spec):
     pos_args = _get_pos_args(spec)
     kargs = _get_kargs(pos_args, spec)
 
-    return (pos_args, kargs)
+    return (spec.args, pos_args, kargs)
 
 def _create(name, module, func):
     spec = inspect.getargspec(func)
-    args, kargs = _get_func_args(spec)
+    args, pos_args, kargs = _get_func_args(spec)
 
     u = FunctionUnit()
     u.name = name
@@ -187,19 +191,100 @@ def _create(name, module, func):
     u.func = func
     u.args = args
     u.kargs = kargs
+    u.pos_args = pos_args
 
     return u
 
-def create_unit_from_dict(name, module, d):
-    func_name = d['func']
-    f = getattr(module, func_name)
-    u = _create(name, module, f)
+def check_func_unit(name, args, kargs, d):
+    
+    tmp_args = args
+    if d['type'] == 'meta':
+        tmp_args = [a for a in args if a != 'ctx']
 
-    if 'input' in d:
-        u.input_desc = d['input']
-    if 'output' in d:
-        u.output = d['output']
+    tmp_args = [a for a in tmp_args if a != 'dryrun']
+
+    if len(tmp_args) > 0:
+        if 'input' not in d or not isinstance(d['input'], dict):
+            raise Exception("Inputs not defined defined (or is not a dict) for unit %s" % name)
+        
+        if len(tmp_args) != len(d['input']):
+            raise Exception("Argument count mismatch between function and configured for unit %s" % name)
+
+        for arg in tmp_args:
+            if arg not in d['input']:
+                raise Exception("Arg [%s] not defined in input for unit %s" % (arg, name))
+
+    tmp_kargs = {k:v for k, v in kargs.iteritems() if k != 'dryrun'}
+    if len(tmp_kargs) > 0:
+        if 'defaults' not in d:
+            raise Exception("Keyword args defined in function but not configured for unit %s" % name)
+
+        if len(tmp_kargs) != len(d['defaults']):
+            raise Exception("Keyword argument count mismatch between function and configured for unit %s" % name)
+
+        for k, v in tmp_kargs.iteritems():
+            if v != d['defaults'][k]:
+                raise Exception("Keyword argument value mismatch for arg %s for unit %s" % (k, name))
+                
+def check_derived_unit(name, d):
+    if 'defaults' in d:
+        if 'input' not in d:
+            raise Exception("Defaults defined but not inputs for unit %s" % name)
+
+        for arg in d['defaults']:
+            if arg not in d['input']:
+                raise Exception("Default arg %s not defined in input for unit %s" % (arg, name))
+    
+def create_function_unit_from_dict(name, d):
+    if 'func' not in d:
+        raise Exception("Key 'func' not defined for unit: %s" % name)
+    
+    func_path = d['func']
+    module_name, func_name = os.path.splitext(func_path)
+    m = importlib.import_module(module_name)
+    func_name = func_name[1:]
+
+    if not hasattr(m, func_name):
+        raise Exception("Function [%s] not found in module [%s] for unit [%s]" % (func_name, module_name, name))
+
+    func = getattr(m, func_name)
+    spec = inspect.getargspec(func)
+    args, pos_args, kargs = _get_func_args(spec)
+
+    check_func_unit(name, args, kargs, d)
+    u = FunctionUnit()
+    u.name = name
+    u.module = m
+    u.func = func
+    u.args = args
+    u.kargs = kargs
+    u.pos_args = pos_args
+
+    if d['type'] == 'simple':
+        u.unit_type = UNIT_TYPE_SIMPLE
+    else:
+        u.unit_type = UNIT_TYPE_META
+
     return u
+
+
+def create_unit_from_dict(name, d):
+    if 'type' not in d:
+        d['type'] = 'simple'
+
+    if 'desc' not in d:
+        raise Exception("Description missing for unit: %s" % name)
+
+    unit_type = d['type']
+    if unit_type in ['simple', 'meta']:
+        u = create_function_unit_from_dict(name, d)
+    elif unit_type == 'derived':
+        u = create_derived_unit_from_dict(name, d)
+    else:
+        raise Exception("Unknown unit type %s for unit %s", name)
+
+    return u
+    
 
 def create_unit(name, module, func_name):
     f = getattr(module, func_name)
@@ -215,3 +300,4 @@ def mark_as_meta_unit(u):
 def is_meta_unit(u):
     return u.unit_type == 'meta'
 
+    
