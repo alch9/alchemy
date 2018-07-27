8edc62634d9ab57d8376060ccbfe82740c662b47
Sudeep Jathar <sudeep.jathar@gmail.com>
2018-07-27 08:27:10 +0530

    New ui

--- webapp/react-app/js/ConfigView.js
+++ webapp/react-app/js/ConfigView.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { Segment, Search, Grid, Button, Card, Table, Container } from 'semantic-ui-react';
+import { Segment, Search, Grid, Button, Card, Table, Header, Divider } from 'semantic-ui-react';
 
 class ConfigView extends React.Component {
     constructor(props) {
@@ -15,6 +15,8 @@ class ConfigView extends React.Component {
         this.handleSearchChange = this.handleSearchChange.bind(this)
         this.handleUnitClick = this.handleUnitClick.bind(this)
         this.getSelectedUnit = this.getSelectedUnit.bind(this)
+        this.renderUnitInput = this.renderUnitInput.bind(this)
+        this.renderUnitOutput = this.renderUnitOutput.bind(this)
         this.state.results = this.matchSource("")
     }
 
@@ -52,6 +54,83 @@ class ConfigView extends React.Component {
         }
     }
 
+    renderUnitInput(unit) {
+        var input_el = []
+        for (var arg in unit.input) {
+            var defval = <div></div>;
+            if('def' in unit.input[arg]) {
+                defval = unit.input[arg].def
+            }
+            var j = (<Table.Row>
+                <Table.Cell>
+                    {arg}
+                </Table.Cell>
+                <Table.Cell>
+                    {defval}
+                </Table.Cell>
+                <Table.Cell>
+                    {unit.input[arg].desc}
+                </Table.Cell>
+                </Table.Row>)
+            
+            input_el.push(j)
+        }
+
+        return (
+            <div>
+            <Header as='h3' attached='top'>
+                Input
+            </Header>
+            <Table celled striped attached>
+                <Table.Header>
+                    <Table.Row>
+                        <Table.HeaderCell>Argument</Table.HeaderCell>
+                        <Table.HeaderCell>Default value</Table.HeaderCell>
+                        <Table.HeaderCell>Description</Table.HeaderCell>
+                    </Table.Row>
+                </Table.Header>
+                <Table.Body>
+                    {input_el}
+                </Table.Body>
+            </Table>
+            </div>)
+    }
+
+    renderUnitOutput(unit) {
+        var output_el = []
+        for (var arg in unit.output) {
+            var j = (<Table.Row>
+                <Table.Cell>
+                    {arg}
+                </Table.Cell>
+                <Table.Cell>
+                    {unit.output[arg]}
+                </Table.Cell>
+                </Table.Row>)
+            
+            output_el.push(j)
+        }
+
+        return (
+            <div>
+            <Header as='h3' attached='top'>
+                Output
+            </Header>
+            <Table celled striped attached>
+                <Table.Header>
+                    <Table.Row>
+                        <Table.HeaderCell>Variable</Table.HeaderCell>
+                        <Table.HeaderCell>Description</Table.HeaderCell>
+                    </Table.Row>
+                </Table.Header>
+                <Table.Body>
+                    {output_el}
+                </Table.Body>
+            </Table>
+            </div>)
+
+    }
+
     getSelectedUnit() {
         var ret = <div></div>
         if(!this.state.selectedUnit) {
@@ -63,18 +142,27 @@ class ConfigView extends React.Component {
         console.log('Selected unit', unit)
         var unit_type = this.getUnitType(unit)
 
+        var input_content = this.renderUnitInput(unit)
+        var output_content = this.renderUnitOutput(unit)
+
         ret = (
-            <Card>
+            <Card fluid>
                 <Card.Content>
                     <Card.Header>
                         {this.state.selectedUnit}
                     </Card.Header>
                     <Card.Meta>{unit_type}</Card.Meta>
                     <Card.Description>
+                        <Segment color="yellow" basic>
                         <pre>
                         {unit.desc}
                         </pre>
+                        </Segment>
                     </Card.Description>
+                    <Divider/>
+                    {input_content}
+                    <Divider/>
+                    {output_content}
                 </Card.Content>
             </Card>
         );
