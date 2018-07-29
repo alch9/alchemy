12ef71a7b694b089117b6b045102aac7518408a1
Sudeep Jathar <sudeep.jathar@gmail.com>
2018-07-29 23:17:09 +0530

    Flows are searchable

--- webapp/react-app/js/ConfigView.js
+++ webapp/react-app/js/ConfigView.js
@@ -2,44 +2,75 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { Segment, Search, Grid, Button, Card, Table, Header, Divider } from 'semantic-ui-react';
+import { Segment, List, Header, Grid, Card, Table, Divider, Input } from 'semantic-ui-react';
 
 class ConfigView extends React.Component {
     constructor(props) {
         super(props);
+
+        var res = this.matchSource("", true)
         this.state = {
-            results: [],
-            selectedUnit: null
+            unitResults: res.unitsResults,
+            flowResults: res.flowResults,
+            selectedUnit: null,
+            selectedFlow: null,
+            selectedItemType: null,
         }
 
         this.handleSearchChange = this.handleSearchChange.bind(this)
         this.handleUnitClick = this.handleUnitClick.bind(this)
-        this.getSelectedUnit = this.getSelectedUnit.bind(this)
+        this.getSelectedItem = this.getSelectedItem.bind(this)
         this.renderUnitInput = this.renderUnitInput.bind(this)
         this.renderUnitOutput = this.renderUnitOutput.bind(this)
         this.renderUnitList = this.renderUnitList.bind(this)
-        this.state.results = this.matchSource("")
+        this.renderFlowList = this.renderFlowList.bind(this)
     }
 
-    matchSource(value) {
-        var results = []
+    componentDidUpdate(prevProps) {
+        if (this.props.configChangeCounter != prevProps.configChangeCounter) {
+            var res = this.matchSource("", true)
+            this.setState({
+                unitResults: res.unitsResults, flowResults: res.flowResults,
+            })
+        }
+    }
+
+    matchSource(value, matchAll) {
+        var unitResults = []
         for (var k in this.props.units) {
-            if (k.indexOf(value) != -1) {
-                results.push({title: k, description: k})
+            var u = this.props.units[k]
+            if (matchAll || k.toLowerCase().indexOf(value.toLowerCase()) != -1 
+                || u.desc.toLowerCase().indexOf(value.toLowerCase()) != -1) {
+                unitResults.push({title: k, description: u.desc})
             }
         }
 
-        return results
+        var flowResults = []
+        for (var k in this.props.flows) {
+            var f = this.props.flows[k]
+            if (matchAll || k.toLowerCase().indexOf(value.toLowerCase()) != -1 
+                || f.desc.toLowerCase().indexOf(value.toLowerCase()) != -1) {
+                flowResults.push({title: k, description: f.desc})
+            }
+        }
+        return {
+            unitsResults: unitResults,
+            flowResults: flowResults,
+        }
     }
 
     handleSearchChange(e, d) {
-        var results = this.matchSource(d.value)
-        this.setState({results: results})
+        var results = this.matchSource(d.value, false)
+        this.setState({unitResults: results.unitsResults, flowResults: results.flowResults})
     }
 
-    handleUnitClick(e, d) {
-        console.log("Unit button", d.uname)
-        this.setState({selectedUnit: d.uname})
+    handleUnitClick(e) {
+        console.log("Unit item", e.target.name, "type", e.target.type)
+        if(e.target.type == "unit") {
+            this.setState({selectedUnit: e.target.name, selectedFlow: null, selectedItemType: e.target.type})
+        }else{
+            this.setState({selectedUnit: null, selectedFlow: e.target.name, selectedItemType: e.target.type})
+        }
     }
 
     getUnitType(unit) {
@@ -53,6 +84,9 @@ class ConfigView extends React.Component {
         if (type == 3) {
             return 'Derived'
         }
+        if (type == 4) {
+            return 'Simple wrap'
+        }
     }
 
     renderUnitInput(unit) {
@@ -61,6 +95,9 @@ class ConfigView extends React.Component {
             var defval = <div></div>;
             if('def' in unit.input[arg]) {
                 defval = unit.input[arg].def
+                if (typeof defval == "string" && defval.length == 0) {
+                    defval = "<empty string>"
+                }
             }
             var j = (<Table.Row key={unit.name + "." + arg}>
                 <Table.Cell>
@@ -132,31 +169,60 @@ class ConfigView extends React.Component {
 
     }
 
-    getSelectedUnit() {
-        var ret = <div></div>
-        if(!this.state.selectedUnit) {
-            return ret
+    getSelectedItem() {
+        var input_content = []
+        var output_content = []
+        var header = ""
+        var meta = ""
+        var desc = ""
+        var renderItem = false
+
+        if (this.state.selectedUnit) {
+            if ( !(this.state.selectedUnit in this.props.units)) {
+                return <div></div>
+            }
+
+            console.log("Unit Info props: ", this.props)
+            var unit = this.props.units[this.state.selectedUnit]
+            console.log('Selected unit', unit)
+            var unit_type = this.getUnitType(unit)
+
+            header = this.state.selectedUnit
+            meta = "Unit: " + unit_type
+            input_content = this.renderUnitInput(unit)
+            output_content = this.renderUnitOutput(unit)
+            desc = unit.desc
+            renderItem = true
         }
 
-        console.log("Unit Info props: ", this.props)
-        var unit = this.props.units[this.state.selectedUnit]
-        console.log('Selected unit', unit)
-        var unit_type = this.getUnitType(unit)
+        if (this.state.selectedFlow) {
+            if ( !(this.state.selectedFlow in this.props.flows)) {
+                return <div></div>
+            }
 
-        var input_content = this.renderUnitInput(unit)
-        var output_content = this.renderUnitOutput(unit)
+            var flow = this.props.flows[this.state.selectedFlow]
 
-        ret = (
+            header = this.state.selectedFlow
+            meta = "Flow"
+            input_content = this.renderUnitInput(flow)
+            output_content = this.renderUnitOutput(flow)
+            desc = flow.desc
+            renderItem = true
+        }
+
+        var ret = <div></div>
+        if(renderItem) {
+            ret = (
             <Card fluid>
                 <Card.Content>
                     <Card.Header>
-                        {this.state.selectedUnit}
+                        {header}
                     </Card.Header>
-                    <Card.Meta>{unit_type}</Card.Meta>
+                    <Card.Meta>{meta}</Card.Meta>
                     <Card.Description>
                         <Segment basic style={{background: '#f9f7e5'}}>
                         <pre>
-                        {unit.desc}
+                        {desc}
                         </pre>
                         </Segment>
                     </Card.Description>
@@ -167,40 +233,79 @@ class ConfigView extends React.Component {
                 </Card.Content>
             </Card>
         );
+        }
 
         return ret
     }
 
     renderUnitList() {
         var ulist = []
-        for (var i in this.state.results) {
-            var title = this.state.results[i].title 
-            ulist.push(<Button uname={title} key={title} onClick={this.handleUnitClick}>{title}</Button>)
+        for (var i in this.state.unitResults) {
+            var title = this.state.unitResults[i].title 
+            var desc = this.state.unitResults[i].description
+            desc = desc.split("\n")[0]
+            ulist.push(
+                <List.Item key={title}>
+                    <List.Content>
+                    <a href="#" type="unit" name={title} onClick={this.handleUnitClick}>{title}</a>
+                    </List.Content>
+                    <List.Description>
+                        {desc}
+                    </List.Description>
+                </List.Item>)
         }
 
-        return ulist
+        return (<List relaxed style={{overflow: "auto"}}>
+            {ulist}
+        </List>)
+    }
+
+    renderFlowList() {
+        var flist = []
+        for (var i in this.state.flowResults) {
+            var title = this.state.flowResults[i].title 
+            var desc = this.state.flowResults[i].description
+            desc = desc.split("\n")[0]
+            flist.push(
+                <List.Item key={title}>
+                    <List.Content>
+                    <a href="#" type="flow" name={title} onClick={this.handleUnitClick}>{title}</a>
+                    </List.Content>
+                    <List.Description>
+                        {desc}
+                    </List.Description>
+                </List.Item>)
+        }
+
+        return (<List relaxed>{flist}</List>)
     }
 
     render () {
-        if(this.props.units == undefined) {
+        if(this.props.units == undefined || this.props.flows == undefined) {
             return <div></div>
         }
         var ulist = this.renderUnitList()
-        var unitInfo = this.getSelectedUnit()
+        var flist = this.renderFlowList()
+        var unitInfo = this.getSelectedItem()
         return (
             <Grid>
                 <Grid.Row>
-                    <Search
-                        minCharacters={2}
-                        onSearchChange={this.handleSearchChange}
-                        results={this.state.results}
+                    <Input
+                        icon='search'
+                        placeholder='Search ...'
+                        onChange={this.handleSearchChange}
                     />
                 </Grid.Row>
                 <Grid.Row columns={2} divided>
-                    <Grid.Column>
-                        <Button.Group vertical={true}>
+                    <Grid.Column width={3}>
+                            <Header as='h5' content='Units'/>
+                            <Divider/>
                             {ulist}
-                        </Button.Group>
+                    </Grid.Column>
+                    <Grid.Column width={3}>
+                            <Header as='h5' content='Flows'/>
+                            <Divider/>
+                            {flist}
                     </Grid.Column>
                     <Grid.Column>
                         {unitInfo}
